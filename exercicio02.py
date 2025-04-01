a0cd5a0358348381bab2098bd8100f78c1e66514
Your Name <you@example.com>
2025-04-01 14:42:40 -0300

    add sinal de igual

--- exercicio02.py
+++ exercicio02.py
@@ -7,7 +7,7 @@ subtracao = numero1-numero2
 multiplicacao = numero1*numero2
 divisao = numero1/numero2
 
-print(f"A soma de {numero1}+{numero2} é {soma}")
-print(f"A subtracao de {numero1}-{numero2} é {subtracao}")
-print(f"A multiplicacao de {numero1}*{numero2} é {multiplicacao}")
-print(f"A divisao de {numero1}/{numero2} é {divisao}")
+print(f"A soma de {numero1}+{numero2} é = {soma}")
+print(f"A subtracao de {numero1}-{numero2} é = {subtracao}")
+print(f"A multiplicacao de {numero1}*{numero2} é = {multiplicacao}")
+print(f"A divisao de {numero1}/{numero2} é = {divisao}")
