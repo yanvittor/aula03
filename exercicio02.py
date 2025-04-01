9f7f956ebe18733a279f58e3e144cda76993b028
Your Name <you@example.com>
2025-04-01 14:38:56 -0300

    usei as quatro operacoes usando dois numeros

---
+++ exercicio02.py
@@ -0,0 +1,13 @@
+# usar dois numeros para as quatros operações
+numero1 = (int(input("digite um número")))
+numero2 = (int(input("digite um número")))
+
+soma = numero1+numero2
+subtracao = numero1-numero2
+multiplicacao = numero1*numero2
+divisao = numero1/numero2
+
+print(f"A soma de {numero1}+{numero2} é {soma}")
+print(f"A subtracao de {numero1}-{numero2} é {subtracao}")
+print(f"A multiplicacao de {numero1}*{numero2} é {multiplicacao}")
+print(f"A divisao de {numero1}/{numero2} é {divisao}")
--- exercio01.py
+++ exercio01.py
@@ -1,8 +1,8 @@
-#ler o programa que inmfrome os dados de uma pessoa:nome, idade e salario
+#ler o programa que informe os dados de uma pessoa:nome, idade e salario
 nome = str(input("digite seu nome"))
 idade = int(input("digite sua idade"))
 salario = float(input("digite seu salario"))
 percentual = float(input("seu salario com base no percentual de"))
-aumento = ((salario)*(percentual)/100)
-salario_aumentado = (salario)+(aumento)
-print(f"Ola,{nome},sua idade é{idade} e seu salario {salario_aumentado:.2f}")
+aumento = salario * percentual/100
+salarionovo = salario+aumento
+print(f"Ola,{nome},sua idade é {idade} e seu salário {salarionovo:.2f}")
