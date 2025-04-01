e1e0d136dc3fc0bccd426be448c27a8bac46c41d
Your Name <you@example.com>
2025-04-01 15:46:08 -0300

    fiz um código para receber dois múmeros e mostrar eles em ordem crescente

---
+++ exercicio04.py
@@ -0,0 +1,8 @@
+#código para receber em múmeros e mostrar eles em ordem crescente
+X  = int(input("digite um numero"))
+Y = int(input("digite um numero"))
+
+if X > Y:
+    print(f"{Y},{X}")
+else:
+    print(f"{X},{Y}")
