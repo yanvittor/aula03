0342b3afec261994bd96033a630f6548a5ac2ac8
Your Name <you@example.com>
2025-04-08 15:26:26 -0300

    Criei um código para receber um número de 1 até 12 e mostrar o nome do mês correspondente

---
+++ exercicio10.py
@@ -0,0 +1,28 @@
+#Código para receber número de 1 até 12 e mostrar o nome do mês correspondente
+num_mes = int(input("Insira o Mês"))
+if num_mes == 1:
+    print(f"Janeiro")
+elif num_mes == 2:
+     print(f"Fevreiro")
+elif num_mes == 3:
+     print(f"Março")
+elif num_mes == 4:
+    print(f"Abril")
+elif num_mes == 5:
+    print(f"Maio")
+elif num_mes == 6:
+    print(f"Junho")
+elif num_mes == 7:
+    print(f"Julho")
+elif num_mes == 8:
+     print(f"Agosto")
+elif num_mes == 9:
+     print(f"Setembro")
+elif num_mes == 10:
+    print(f"Outubro")
+elif num_mes == 11:
+    print(f"Novembro")
+elif num_mes == 12:
+    print(f"Dezembro")
+else:
+    print(f"Mês Inválido")
